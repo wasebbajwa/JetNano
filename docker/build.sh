8c3d34c146ab87e61a5ce12e18009150aa8cd710
wasebbajwa <70357685+wasebbajwa@users.noreply.github.com>
2021-11-27 15:33:56 -0800

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/env bash
-#
-# This script builds the jetson-inference docker container from source.
-# It should be run from the root dir of the jetson-inference project:
-#
-#     $ cd /path/to/your/jetson-inference
-#     $ docker/build.sh
-#
-# Also you should set your docker default-runtime to nvidia:
-#     https://github.com/dusty-nv/jetson-containers#docker-default-runtime
-#
 
 BASE_IMAGE=$1
 
